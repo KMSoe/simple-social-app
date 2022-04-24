e587d0b307fdd1ca920c909672b86b17654afb31
KyiThantSin <kyithantsin@mtu.edu.mm>
2022-04-24 14:39:50 +0630

    routes-setup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import PostList from './features/posts/PostList';
+import {Route, Routes , BrowserRouter as Router} from "react-router-dom"
+import PostDetails from './features/posts/PostDetails';
 
 function App() {
   return (
     <Router>
       <h1>Blog App</h1>
-      <PostList />
+      <Routes>
+        <Route exact path="/posts" element={<PostList />} />
+        <Route exact path="/posts/:id" element={<PostDetails/>} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+
